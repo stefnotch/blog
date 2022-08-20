1bac3200c3bbed88100259a0cfb1ece872b8ef28
stefnotch <brandmairstefan@gmail.com>
2022-08-20 20:49:57 +0200

    Add depth first generator for getting solutions

--- docs/NinetyDegreesPuzzle/NinetyDegreesPuzzle.vue
+++ docs/NinetyDegreesPuzzle/NinetyDegreesPuzzle.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import { createBoard, Board as GenericBoard, Cell } from "../Puzzles/board";
+import {
+  createBoard,
+  Board as GenericBoard,
+  Cell,
+  copyBoard,
+} from "../Puzzles/board";
 
 const Cells = {
   Empty: 0,
@@ -11,7 +16,15 @@ const Cells = {
   TopBottom: 5,
   LeftRight: 6,
   All: 7,
-};
+} as const;
+
+const Directions = {
+  Up: 0,
+  Right: 1,
+  Down: 2,
+  Left: 3,
+} as const;
+type Direction = typeof Directions[keyof typeof Directions];
 
 function createBoardWithSize(size: { width: number; height: number }) {
   return createBoard(Cells, {
@@ -29,6 +42,108 @@ const boardSize = ref({
   height: 10,
 });
 const board = ref(createBoardWithSize(boardSize.value));
+
+function cellToDirection(cell: Cell<typeof Cells>): Direction[] {
+  switch (cell) {
+    case Cells.TopLeft:
+      return [Directions.Up, Directions.Left];
+    case Cells.LeftBottom:
+      return [Directions.Down, Directions.Left];
+    case Cells.BottomRight:
+      return [Directions.Down, Directions.Right];
+    case Cells.RightTop:
+      return [Directions.Up, Directions.Right];
+    case Cells.TopBottom:
+      return [Directions.Up, Directions.Down];
+    case Cells.LeftRight:
+      return [Directions.Left, Directions.Right];
+    case Cells.All:
+      return [
+        Directions.Up,
+        Directions.Left,
+        Directions.Down,
+        Directions.Right,
+      ];
+    case Cells.Empty:
+      return [];
+    default:
+      throw new Error("Unknown cell " + cell);
+  }
+}
+
+function oppositeDirection(direction: Direction): Direction {
+  switch (direction) {
+    case Directions.Up:
+      return Directions.Down;
+    case Directions.Right:
+      return Directions.Left;
+    case Directions.Down:
+      return Directions.Up;
+    case Directions.Left:
+      return Directions.Right;
+    default:
+      throw new Error("Unknown direction " + direction);
+  }
+}
+
+function getCellOrNull(board: Board, x: number, y: number) {
+  if (x < 0 || x >= board.width || y < 0 || y >= board.height) {
+    return null;
+  }
+  return board.cells[y][x];
+}
+
+function neighborsAt(
+  board: Board,
+  x: number,
+  y: number
+): { [key in Direction]: Cell<typeof Cells> } {
+  return {
+    [Directions.Up]: getCellOrNull(board, x, y - 1) ?? Cells.Empty,
+    [Directions.Right]: getCellOrNull(board, x + 1, y) ?? Cells.Empty,
+    [Directions.Down]: getCellOrNull(board, x, y + 1) ?? Cells.Empty,
+    [Directions.Left]: getCellOrNull(board, x - 1, y) ?? Cells.Empty,
+  };
+}
+
+function mapValues<T extends Record<any, any>, U>(
+  obj: T,
+  fn: (v: keyof T) => U
+): { [key in keyof T]: U } {
+  return Object.fromEntries(
+    Object.entries(obj).map(([k, v]) => [k, fn(v)])
+  ) as any;
+}
+
+function validCellsAt(
+  board: Board,
+  x: number,
+  y: number
+): Cell<typeof Cells>[] {
+  const cell = board.cells[y][x];
+  if (cell === Cells.Empty) {
+    const neighbors = mapValues(neighborsAt(board, x, y), (v) =>
+      cellToDirection(v)
+    );
+    const possibleCells = [
+      Cells.TopLeft,
+      Cells.LeftBottom,
+      Cells.BottomRight,
+      Cells.RightTop,
+      Cells.TopBottom,
+      Cells.LeftRight,
+      Cells.All,
+    ];
+    return possibleCells.filter((v) => {
+      const cellDirections = cellToDirection(v);
+      return cellDirections.every((direction) =>
+        neighbors[direction].includes(oppositeDirection(direction))
+      );
+    });
+  } else {
+    return [];
+  }
+}
 
 function displayCellClass(cell: Cell<typeof Cells>): string {
   if (cell === Cells.Empty) {
@@ -51,7 +166,79 @@ function displayCellClass(cell: Cell<typeof Cells>): string {
   return "";
 }
 
-function handleClick(event: MouseEvent, y: number, x: number) {
+// Incremental computation would be awesome
+// Having a 2D array of cells
+// => Getting a 2D array of valid cells (aka if the above was an empty cell, which cell could I place here)
+// Then changing the 2D array of cells
+// => And only the relevant bits of the 2D array of valid cells would be updated
+
+function nextBestStep(board: Board): {
+  x: number;
+  y: number;
+  possibleCells: Cell<typeof Cells>[];
+} {
+  const nextCells = {
+    score: Infinity,
+    x: 0,
+    y: 0,
+    possibleCells: [] as Cell<typeof Cells>[],
+  };
+  // Pick a nice tile, generate all possibilities and either return them or put them in the queue
+  for (let y = 0; y < board.height; y++) {
+    for (let x = 0; x < board.width; x++) {
+      const possibleCells = validCellsAt(board, x, y);
+      if (possibleCells.length == 0) continue;
+      if (possibleCells.length == 1) {
+        return {
+          x,
+          y,
+          possibleCells,
+        };
+      } else if (possibleCells.length < nextCells.score) {
+        nextCells.score = possibleCells.length;
+        nextCells.x = x;
+        nextCells.y = y;
+        nextCells.possibleCells = possibleCells;
+      }
+    }
+  }
+  return nextCells;
+}
+
+/**
+ * Takes a valid board, and returns valid board solutions
+ */
+function* getSolutions(startingBoard: Board) {
+  const queue = [startingBoard];
+
+  if (isFull(startingBoard)) {
+    yield startingBoard;
+    return;
+  }
+
+  while (true) {
+    const board = queue.pop();
+    if (board === undefined) {
+      return;
+    }
+    const step = nextBestStep(board);
+    step.possibleCells.forEach((v) => {
+      const newBoard = copyBoard(board);
+      newBoard.cells[step.y][step.x] = v;
+      if (isFull(newBoard)) {
+        yield newBoard;
+      } else {
+        queue.push(newBoard);
+      }
+    });
+  }
+}
+
+function isFull(board: Board): boolean {
+  return board.cells.every((row) => row.every((cell) => cell !== Cells.Empty));
+}
+
+function handleClick(event: MouseEvent, x: number, y: number) {
   board.value.cells[y][x] = cycleCell(board.value.cells[y][x]);
 }
 
@@ -78,6 +265,7 @@ function cycleCell(cell: Cell<typeof Cells>): Cell<typeof Cells> {
 </script>
 <template>
   <div>
+    <!--
     Playing field size
     <input
       type="number"
@@ -94,11 +282,12 @@ function cycleCell(cell: Cell<typeof Cells>): Cell<typeof Cells> {
       :size="3"
       v-model="boardSize.height"
     />
+    -->
   </div>
   <table :tabIndex="0" className="game-table">
     <tbody>
       <tr v-for="(row, y) in board.cells">
-        <td v-for="(cell, x) in row" @click="(ev) => handleClick(ev, y, x)">
+        <td v-for="(cell, x) in row" @click="(ev) => handleClick(ev, x, y)">
           <div class="game-table-cell" :class="displayCellClass(cell)"></div>
         </td>
       </tr>
