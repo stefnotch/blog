67b8385771b5b04abdd3fe9925c718bb8f195afe
stefnotch <brandmairstefan@gmail.com>
2023-03-02 10:11:23 +0100

    Add search plugin

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,15 +1,6 @@
-//vite.config.ts
-import { SearchPlugin } from "vitepress-plugin-search";
+import SimpleSearch from "vitepress-plugin-simple-search";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [
-    SearchPlugin({
-      // https://github.com/nextapps-de/flexsearch#options
-      preset: "default",
-      previewLength: 62,
-      buttonLabel: "Search",
-      placeholder: "Search docs",
-    }),
-  ],
+  plugins: [SimpleSearch.SimpleSearch({})],
 });
